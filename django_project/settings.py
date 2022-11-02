21d46800ebef6fe95669a85f6bcfbc1d244346cf
Jose Camilo <jrodriguez_131@hotmail.com>
2022-11-01 23:11:03 -0500

    agregando django-environ

--- django_project/settings.py
+++ django_project/settings.py
@@ -1,8 +1,9 @@
 from pathlib import Path
-from environs import Env
+import os
+import environ
 
-env = Env()
-env.read_env()
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -12,10 +13,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', default=False)
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['up.railway.app', 'localhost', '127.0.0.1']
 
