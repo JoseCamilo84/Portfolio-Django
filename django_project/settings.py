2a8003763f7f4d3926064018bf08ffc15a683750
Jose Camilo <jrodriguez_131@hotmail.com>
2022-11-03 16:01:21 -0500

    modificando settings

--- django_project/settings.py
+++ django_project/settings.py
@@ -1,9 +1,8 @@
 from pathlib import Path
-import os
-import environ
+from environs import Env
 
-env = environ.Env()
-environ.Env.read_env()
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,10 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = env.bool("DEBUG", default=False)
 
 CSRF_TRUSTED_ORIGINS = [
     'https://portfolio-django-production-614b.up.railway.app']
@@ -119,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
